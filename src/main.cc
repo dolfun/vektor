b933e3339f36d71fd9559376019edba8349cde9d
dolfun <abhinav150104@gmail.com>
2024-06-29 06:16:18 +0530

    Add command line arguments parsing

--- src/main.cc
+++ src/main.cc
@@ -1,16 +1,45 @@
+#include <map>
+#include <string>
 #include <iostream>
 #include "tracer.h"
 #include "renderer.h"
 #include "canny_edge_detector.h"
 
-int main() {
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    return -1;
+  }
+
+  std::map<std::string, std::string> args;
+  args["-o"] = "output.png";
+  args["-s"] = "1.0";
+
+  for (int i = 2; i < argc; ++i) {
+    if (i + 1 < argc && argv[i][0] == '-') {
+      args[std::string{ argv[i] }]  = std::string { argv[i + 1] };
+    } else {
+      args[std::string{ argv[i] }] = "";
+    }
+  }
 
   try {
-    auto image = Image::load("NobitaNobi.png", Canny::padding_requirement);
+    std::string path { argv[1] };
+    auto image = Image::load(path.c_str(), Canny::padding_requirement);
     auto canny_result = Canny::detect_edges(image);
     auto curves = Tracer::trace(canny_result);
-    auto result = Renderer::render_color(image.width(), image.height(), curves, image);
-    Image::save_as_png(result, "output.png");
+
+    int width = image.width() * std::stof(args["-s"]);
+    int height = image.height() * std::stof(args["-s"]);
+
+    std::string output_path = args["-o"];
+    if (args.contains("-c")) {
+      auto result = Renderer::render_color(width, height, curves, image);
+      Image::save_as_png(result, output_path.c_str());
+
+    } else {
+      auto result = Renderer::render_greyscale(width, height, curves);
+      Image::save_as_png(result, output_path.c_str());
+    }
     
   } catch (const std::exception& e) {
     std::cerr << "Exception occured: {}" << e.what() << std::endl;
