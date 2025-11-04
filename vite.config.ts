fea0e7ecf4f3b4e7fd59f06b12f31c8160e2a635
dolfun <abhinav150104@gmail.com>
2025-11-04 06:53:16 +0530

    chore: add github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/vektor/",
   plugins: [react()],
   resolve: {
     alias: {
